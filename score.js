16d88642b9ade41b95655788874890c41f24061b
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-07 13:00:48 +0530

    Create score.js

---
+++ score.js
@@ -0,0 +1,32 @@
+if(ball!=null)
+    {
+       ball.display();
+        
+        if (ball.body.position.y>760)
+        {
+              if (ball.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  ball=null;
+                  if ( count>= 5) gameState ="end";                          
+              }
+
+
+              else if (ball.body.position.x < 600 && ball.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    ball=null;
+                    if ( count>= 5) gameState ="end";
+
+              }
+              else if (ball.body.position.x < 900 && ball.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    ball=null;
+                    if ( count>= 5)  gameState ="end";
+
+              }      
+              
+        }
+  
+      }
